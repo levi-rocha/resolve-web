3db8f8eece69bd6dfb4d51d01acfe76cb6bbdd5b
unknown <jessica.p.jorge.s@gmail.com>
2017-11-19 16:33:53 -0300

    Issue 060 - Consertar bugs do perfil (página de post do usuário)

--- app/components/posts/user-posts-list.component.ts
+++ app/components/posts/user-posts-list.component.ts
@@ -1,20 +1,33 @@
 import {Component, OnInit} from "@angular/core";
 import { Router, ActivatedRoute } from "@angular/router";
-import {Post} from "../../models/post";
-import {PostService} from "../../services/post-service";
+import { Post } from "../../models/post";
+import { PostService } from "../../services/post-service";
 import { UserPostsService } from "../../services/user-posts.service";
+import { NgProgressService } from "ngx-progressbar";
 
 @Component({
     selector: 'user-posts-list',
     templateUrl: 'app/views/post/user-posts-list.component.html',
-    providers: [PostService]
+    providers: [PostService, UserPostsService]
 })
 export class UserPostsListComponent implements OnInit {
 
     private listUserPosts: Post[];
+
+    error: string;
+    
+	private page: number = 0;
+    
+    private pageSize: number = 5;
+    
+    private criteria: string;
+    
+    private searchInput: string;
     
     constructor(private userPostsService:UserPostsService,
-        private _activatedRoute: ActivatedRoute){}
+        private _activatedRoute: ActivatedRoute,
+        private progressService: NgProgressService,
+        private postService: PostService){}
     
     ngOnInit(){
 
@@ -24,5 +37,38 @@ export class UserPostsListComponent implements OnInit {
             .subscribe(res => {
                 this.listUserPosts = res;
             });
+    }
+
+    refreshList() {
+        this.progressService.start();
+        this.postService.list(this.pageSize, this.page, this.criteria,
+            this.searchInput).subscribe(
+            data => {
+                this.listUserPosts = data;
+                this.progressService.done();
+            },
+            error => {
+                this.error = "Could not list posts";
+                this.progressService.done();
+            }
+        );
+    }
+
+    getUserPostsLength(): number {
+		if (this.listUserPosts != null)
+			return this.listUserPosts.length;
+		return 0;
+	}
+
+
+    previousPage() {
+        this.page--;
+        this.refreshList();
+    }
+
+    
+    nextPage() {
+        this.page++;
+        this.refreshList();
     }
 }
