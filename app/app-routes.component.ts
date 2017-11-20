06f91301a02f43858c77de5c07faf7b4cfd0b306
unknown <jessica.p.jorge.s@gmail.com>
2017-11-19 21:42:41 -0300

    Alterar nomes que não foram alterados anteriormente

--- app/app-routes.component.ts
+++ app/app-routes.component.ts
@@ -1,5 +1,5 @@
 import { Routes } from "@angular/router";
-import { PostRoutes } from "./components/posts/post-routes";
+import { PostRoutes } from "./components/posts/post-routes.component";
 
 export const APP_ROUTES: Routes = [
 	...PostRoutes
